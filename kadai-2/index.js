8fdeaa04f4358e49a41f022dee8ddb71bd8f7d6f
ichidamihooo <ichida.miho5@gmail.com>
2019-11-13 05:03:13 +0000

    test

---
+++ kadai-2/index.js
@@ -0,0 +1,13 @@
+const firstName = 'Miho'
+const secondName = 'Ichida'
+console.log(firstName + secondName)
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const hasDog = false
+const hasCat = true
+console.log(hasDog || hadCat)
+console.log(hadDog && hasCat)
+// 犬も猫も飼っていない
+console.log(!hasDog && !hasCat)
